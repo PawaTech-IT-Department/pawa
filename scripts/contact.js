d00b43ef1ff2b54af358f0aa4cff53ab29c7e8ac
Micheal Juma <119407218+Mikelly-Mikey@users.noreply.github.com>
2025-07-02 10:04:38 +0300

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -19,19 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // --- "Get in Touch" Section ---
-  const emailLink = document.querySelector('a[href="mailto:support@pawa.com"]');
+  const emailLink = document.querySelector('a[href="mailto:pawatech56@gmail.com"]');
   if (emailLink) {
     emailLink.addEventListener("click", function (event) {
       event.preventDefault();
-      window.location.href = "mailto:support@pawa.com";
+      window.location.href = "mailto:pawatech56@gmail.com";
     });
   }
 
-  const phoneLink = document.querySelector('a[href="tel:+254-123-4567"]'); //I am not sure if the contact number is correct, that's why it got a comment here
+  const phoneLink = document.querySelector('a[href="tel:+254748881679"]'); //I am not sure if the contact number is correct, that's why it got a comment here
   if (phoneLink) {
     phoneLink.addEventListener("click", function (event) {
       event.preventDefault();
-      window.location.href = "tel:+254-123-4567";
+      window.location.href = "tel:+254748881679";
     });
   }
 
@@ -44,9 +44,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const socialNetwork = this.getAttribute("aria-label");
         let destination = "/"; // Default destination
 
-        switch (socialNetwork) {
+        switch (socialNetwork) { //at this point, we need to create accounts for the respective networks 
           case "Instagram":
             destination = "https://www.instagram.com";
+            break;
+          case "LinkedIn":
+            destination = "https://www.linkedin.com";
             break;
           case "Twitter":
             destination = "https://www.twitter.com";
@@ -55,8 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
             destination = "https://www.facebook.com";
             break;
           case "Whatsapp":
-            // Replace with your actual WhatsApp link
-            destination = "https://wa.me/2541234567";
+            destination = "https://wa.me/254748881679";
             break;
         }
         window.open(destination, "_blank"); // Open in a new tab
@@ -68,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const callNowButton = document.querySelector(".btn--primary .fa-phone");
   if (callNowButton) {
     const callNowAnchor = callNowButton.parentElement;
-    callNowAnchor.setAttribute("href", "tel:+254-123-4567");
+    callNowAnchor.setAttribute("href", "tel:+254748881679");
   }
 
   const liveChatButton = document.querySelector(".btn--secondary .fa-comments");// talking about live chat,,what progress is made on the bot?! 
